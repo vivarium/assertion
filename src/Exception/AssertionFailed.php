a88e2f3cd4f47d03954f608937a509b249fda845
Luca Cantoreggi <luca@cn7r66.com>
2021-06-04 21:53:13 +0200

    Add custom exception

---
+++ src/Exception/AssertionFailed.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Vivarium
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2021 Luca Cantoreggi
+ */
+
+declare(strict_types=1);
+
+namespace Vivarium\Assertion\Exception;
+
+use InvalidArgumentException;
+
+final class AssertionFailed extends InvalidArgumentException
+{
+}
